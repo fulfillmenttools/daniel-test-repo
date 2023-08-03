2b4e6039e50f7a9cf33cfe5182f5038e2fb22a32
Daniel Schönfelder <schoeda@users.noreply.github.com>
2023-08-03 15:12:39 +0200

    a

--- .github/workflows/scripts/stageDoesntExistsCheck.sh
+++ .github/workflows/scripts/stageDoesntExistsCheck.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-appVersion="${1:}"
-stage="${2:}"
-appBucketFolderName="${3:}"
+appVersion="${1}"
+stage="${2}"
+appBucketFolderName="${3}"
 releaseBucketUrl="gs://ocff-deployment-mobileapps/${appBucketFolderName}/releases/${appVersion}"
 
 rawLs="$(gcloud storage ls "${releaseBucketUrl}")"
 grepStageUrl=$(echo "${rawLs}" | grep "/${stage}" || echo "NOT_EXISTS")
 
-if [[ "${grepStageUrl}" == "NOT_EXISTS" ]]
-then
+if [[ "${grepStageUrl}" == "NOT_EXISTS" ]]; then
   echo "App ${appVersion} in ${stage} stage not available."
 else
   echo "${stage} stage app found in ${releaseBucketUrl}"
-    echo "DEBUG ls content: ${rawLs}"
-    exit 1
+  echo "DEBUG ls content: ${rawLs}"
+  exit 1
 fi
