6736349b5728e03d00766854f76a0eb1a0571b7d
Daniel Schönfelder <schoeda@users.noreply.github.com>
2023-09-29 11:47:28 +0200

    faw

--- .github/workflows/scripts/stageDoesntExistsCheck.sh
+++ .github/workflows/scripts/stageDoesntExistsCheck.sh
@@ -7,10 +7,11 @@ appVersion="${1}"
 stage="${2}"
 packageAppName="${3}"
 releaseBucketUrl="gs://ocff-deployment-mobileapps/${packageAppName}/android/releases/${appVersion}-${stage}"
-
+echo $releaseBucketUrl
 rawLs="$(gcloud storage ls "${releaseBucketUrl}")"
+echo $rawLs
 stageExists=$(echo "${rawLs}" | grep "${appVersion}-${stage}" || echo "NOT_EXISTS")
-
+echo $stageExists
 if [[ "${stageExists}" == "NOT_EXISTS" ]]; then
   echo "No application with version ${appVersion} in ${stage} stage available."
 else
